a1a1792fbcb8559009a22b9289a724aa7adc288f
marco <marco.dutra99@hotmail.com>
2021-12-09 19:44:33 -0300

    Commit 6

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,19 @@
 import express from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
+// index, show, create, update, delete
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/items', async (request, response) => {
-    const items = await knex('items').select('*');
-    return response.json(items);
-});
+routes.get('/items', itemsController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points:id', pointsController.show);
+
+
+//INDEX = listagem, show = exibir um unico registro daquele, 
 
 export default routes;
