b4c2410a7b28cc983c4ae968bdd12209955aba3f
marco <marco.dutra99@hotmail.com>
2021-12-09 10:53:05 -0300

    5

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -1,7 +1,7 @@
 import Knex from 'knex';
 
-export async function name(knex : Knex) {
-    knex('itens').insert([
+export async function seed(knex : Knex) {
+    await knex('items').insert([
         {title: 'Lâmpadas', image:'lampadas.svg'},
         {title: 'Pilhas e Baterias', image:'baterias.svg' },
         {title: 'Papéis e Papelão', image:'papeis-papelao.svg' },
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import knex from './database/connection';
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({Message : 'Hello Word'});
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+    return response.json(items);
 });
 
 export default routes;
