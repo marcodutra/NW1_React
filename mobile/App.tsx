78a7d8715de429302849081a233e092dc23ef904
marco <marco.dutra99@hotmail.com>
2021-12-17 13:01:53 -0300

    Construção da pagina Points

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -6,8 +6,7 @@ import {Roboto_400Regular, Roboto_500Medium} from '@expo-google-fonts/roboto'
 import {Ubuntu_700Bold, useFonts} from '@expo-google-fonts/ubuntu'
 
 
-
-import Home from './src/pages/Home';
+import Routes from './src/routes';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
@@ -23,7 +22,7 @@ export default function App() {
   return (
     <>
     <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-    <Home/>
+    <Routes/>
     </>
   );
 }
